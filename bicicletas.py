4d03dc71e8a508bcf128c3666c9395b609987675
Jose Acuña <83027971+jose-duoc@users.noreply.github.com>
2026-05-25 21:41:44 -0400

    Add files via upload

--- bicicletas.py
+++ bicicletas.py
@@ -53,3 +53,6 @@ while ejecutando:
                 print(f"Devolución exitosa ha regresado {cantidad_a_devolver} bicicletas")
         except ValueError:
             print("Error: Debe ingresar un número entero válido")
+    #opcion 4: Viajes activos
+    elif opcion == 4:
+        print(f"\n[HISTORIAL] actualmente hay {viajes_activos} bicicleta(s) en uso por usuarios")
